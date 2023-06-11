760a9cdee8fd6f37a49c64fdf440922a0d0569cb
Rafael <reis.rafael18@gmail.com>
2023-06-11 10:23:09 -0300

    footer commented

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,18 +6,14 @@ import {
   useColorModeValue,
 } from "@chakra-ui/react";
 
+//Exporta componente de footer
 export default function Footer() {
   return (
     <Box
       bg={useColorModeValue("gray.50", "gray.900")}
       color={useColorModeValue("gray.700", "gray.200")}
     >
-      <Container
-        as={Stack}
-        maxW={"6xl"}
-        py={4}
-        align="center"
-      >
+      <Container as={Stack} maxW={"6xl"} py={4} align="center">
         <Text>© 2023 Eldora Boo. All rights reserved</Text>
       </Container>
     </Box>
